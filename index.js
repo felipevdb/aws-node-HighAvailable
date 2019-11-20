f13607f6cb6686e7cbe7311b3eb7592952a11ec6
felipevdb <felipevdb@gmail.com>
2019-11-20 10:40:16 -0300

    Get instance Id function implemented

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
-var express = require('express')
-var http = require('http');
+const express = require('express'),
+    http = require('http');
+    path = require('path');
+    AWS = require('aws-sdk');
+
 
 var app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.sendFile(path.join(__dirname, 'Client', 'index.html'))
 })
+
+
+app.get('/getMachineId', function (req, res){
+    var meta  = new AWS.MetadataService();
+
+    var machineId =  meta.request("/latest/meta-data/instance-id", function(err, data){
+    console.log(data);
+    });
+
+    res.send({message: machineId});
+}),
+
+
 
 app.listen(8000, function () {
   console.log('Example app listening on port 8000!')
 })
+
+
+
+
+//MetadataService
