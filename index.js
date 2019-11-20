05ee25acde4e1478e40023e0db8a372a60d57a85
felipevdb <felipevdb@gmail.com>
2019-11-19 23:15:20 -0300

    Project Creation

---
+++ index.js
@@ -0,0 +1,12 @@
+var express = require('express')
+var http = require('http');
+
+var app = express()
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+app.listen(8000, function () {
+  console.log('Example app listening on port 8000!')
+})
